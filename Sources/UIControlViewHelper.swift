404c9014d30278bf94e442e27486ccf959aa728b
Vlad Karlugin <50109143+VladK9@users.noreply.github.com>
2021-10-02 11:31:38 +0500

    Update UIControlViewHelper.swift

--- Sources/UIControlViewHelper.swift
+++ Sources/UIControlViewHelper.swift
@@ -115,3 +115,13 @@ extension UIView {
     }
     
 }
+
+extension UIColor {
+    
+    var isLight: Bool {
+        var white: CGFloat = 0
+        getWhite(&white, alpha: nil)
+        return white > 0.8
+    }
+    
+}
