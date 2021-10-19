37278f67af863edadcd2a1b228f8b4565397a728
Vlad Karlugin <50109143+VladK9@users.noreply.github.com>
2021-10-19 23:36:33 +0500

    Update UIControlViewHelper.swift

--- Sources/UIControlViewHelper.swift
+++ Sources/UIControlViewHelper.swift
@@ -118,10 +118,32 @@ extension UIView {
 
 extension UIColor {
     
+    //MARK: - isLight
     var isLight: Bool {
         var white: CGFloat = 0
         getWhite(&white, alpha: nil)
         return white > 0.8
+    }
+    
+    //MARK: - lighter/darker
+    func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: abs(percentage) )
+    }
+    
+    func darker(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+    
+    func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: min(red + percentage/100, 1.0),
+                           green: min(green + percentage/100, 1.0),
+                           blue: min(blue + percentage/100, 1.0),
+                           alpha: alpha)
+        } else {
+            return nil
+        }
     }
     
 }
